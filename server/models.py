cb260614fb5818b480a03ae8604f424aaf1abcba
AlvinVJ <alvinvinodjohn123@gmail.com>
2024-10-03 21:01:43 +0530

    hi

---
+++ server/models.py
@@ -0,0 +1,18 @@
+from app import db
+
+class user_credentials(db.Model):
+    __tablename__ = 'user_credentials'
+
+    #defining the columns of the table
+
+    uid = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+
+    # defining the relationship thing.(what to display when we print the object
+    
+    def __repr__(self):
+        return {'uid': self.uid, 'username': self.username, 'password': self.password}
+    
+
+    
---
+++ server/routes.py
@@ -0,0 +1,2 @@
+from flask import render_template, request
+from models import user_credentials
