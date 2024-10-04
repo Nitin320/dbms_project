24fe4e0577dbd9c3790986648b1110af89b199ca
AlvinVJ <alvinvinodjohn123@gmail.com>
2024-10-04 23:14:14 +0530

    implemented sign in backend logic

--- client/main/src/test.js
+++ client/main/src/test.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Test = () => {
+    const [text, setText] = useState('');
+
+    useEffect(() => {
+        const fetchText = async () => {
+            try {
+                const response = await fetch('http://127.0.0.1:5000/api/cred');
+                const data = await response.json();
+                setText(data.password);
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
+        };
+
+        fetchText();
+    }, []);
+
     return (
         <div className="flex items-center justify-center h-screen">
-            <h1 className="text-6xl font-bold">hello</h1>
+            <h1 className="text-6xl font-bold">{text}</h1>
         </div>
     );
 };
