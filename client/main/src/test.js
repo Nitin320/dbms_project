df4dfef0c909b6fb82fe147a719bbaa93d4b1b1d
AlvinVJ <alvinvinodjohn123@gmail.com>
2024-10-04 21:39:42 +0530

    nothing to say

---
+++ client/main/src/test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Test = () => {
+    return (
+        <div className="flex items-center justify-center h-screen">
+            <h1 className="text-6xl font-bold">hello</h1>
+        </div>
+    );
+};
+
+export default Test;
